e502df5ebfef5009cbc00b0c358ed46b71abe54b
Joe Brady <joe.brady@uk.ibm.com>
2018-09-17 19:07:50 +0100

    Add routing for /User-testing-flows/opening-days-start

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,20 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.post('/opening-redirect', function (req, res) {
+  const value = req.body.opening_days;
+  console.log(value)
+  if (value === "every-day") {
+    res.redirect("/")
+  }
+  if (value === "some-days") {
+    res.redirect("/User-testing-flows/opening-days-some-days")
+  }
+  if (value === "irregular-days") {
+    res.redirect("/User-testing-flows/opening-days-irregular")
+  }
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
